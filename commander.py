b27811c418ce9325a0f23c65518ed8dbb2183e6b
Marc <mcavigli@gmail.com>
2022-05-18 22:29:08 -0700

    Replaced hikari with discord.py

--- commander.py
+++ commander.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
+import discord
+import os
+from dotenv import load_dotenv
 
-import hikari
-from authtoken import *
+load_dotenv()
+bot = discord.Client()
 
-bot.run()
+@bot.event
+async def on_ready():
+    print('{0.user} has successfully logged in!'.format(bot))
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Howdy, pardner.')
+
+bot.run(os.getenv('TOKEN'))
