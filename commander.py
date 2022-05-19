3063f1eff6df79cca7675b498a19d48980a6db09
Marc <mcavigli@gmail.com>
2022-05-18 23:14:09 -0700

    Added ping event

--- commander.py
+++ commander.py
@@ -16,6 +16,10 @@ async def on_message(message):
         return
 
     if message.content.startswith('!hello'):
-        await message.channel.send('Howdy, pardner.')
+        await message.channel.send('Howdy, {0}.'.format(message.author.name))
+
+    if message.content.startswith('!ping'):
+        time = message.content[5:]
+        await message.channel.send('@everyone raid starts in {} minutes!'.format(time))
 
 bot.run(os.getenv('TOKEN'))
