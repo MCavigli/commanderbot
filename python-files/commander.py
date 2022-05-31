e033b2881338256a6050f550485240a6ae646659
Marc <mcavigli@gmail.com>
2022-05-30 22:35:42 -0700

    Put main code into a function

--- python-files/commander.py
+++ python-files/commander.py
@@ -3,30 +3,35 @@ import discord
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-bot = discord.Client()
+def main():
+    load_dotenv()
+    bot = discord.Client()
 
-@bot.event
-async def on_ready():
-    print('{0.user} has successfully logged in!'.format(bot))
+    @bot.event
+    async def on_ready():
+        print('{0.user} has successfully logged in!'.format(bot))
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
+    @bot.event
+    async def on_message(message):
+        if message.author == bot.user:
+            return
 
-    if message.content.startswith('!hello'):
-        await message.channel.send('Howdy, {0}.'.format(message.author.name))
+        if message.content.startswith('!hello'):
+            await message.channel.send('Howdy, {0}.'.format(message.author.name))
 
-    if message.content.startswith('!ping'):
-        if len(message.content) == 5:
-            await message.channel.send('@everyone raid starts soon!')
-        else:
-            time = message.content[5:]
-            await message.channel.send('@everyone raid starts in {} minutes!'.format(time))
+        if message.content.startswith('!ping'):
+            if len(message.content) == 5:
+                await message.channel.send('@everyone raid starts soon!')
+            else:
+                time = message.content[5:]
+                await message.channel.send('@everyone raid starts in {} minutes!'.format(time))
 
-    if message.content.startswith('!create'):
+        if message.content.startswith('!create'):
 #        print(message.member.guild_permissions)
-        print(message.author.roles)
+            print(message.author.roles)
 
-bot.run(os.getenv('TOKEN'))
+    bot.run(os.getenv('TOKEN'))
+
+if __name__ == '__main__':
+    main()
+    
